ed02fbf63e5a47b06c333bb4bd1feacbcb3d9906
Ogawa <3223mq118@gmail.com>
2025-06-02 07:04:42 +0900

    a

--- harmonic.c
+++ harmonic.c
@@ -1,3 +1,34 @@
 #include <stdio.h>
 #include "harmonic.h"
 
+Parameters* input_parameters(Parameters *prm){
+    file_output(&prm);
+}
+
+transition_decision(Parameters *prm){
+    file_output(&prm);
+}
+
+r_greater_zero(Parameters *prm){
+    file_output(&prm);
+}
+
+r_less_zero(Parameters *prm){
+    file_output(&prm);
+}
+
+r_equal_w(Parameters *prm){
+    file_output(&prm);
+}
+
+r_equal_zero(Parameters *prm){
+    file_output(&prm);
+}
+
+file_output(Parameters *prm){
+    file_output(&prm);
+}
+
+
+
+
